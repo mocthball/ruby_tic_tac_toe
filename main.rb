d4697b3dd695bfcc9f2ec9b8a68bea1f8d16ed9b
mocthball <56953651+mocthball@users.noreply.github.com>
2023-09-22 07:39:11 +1000

    game play method adjusted

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ class Person
   def initialize(name, token)
     @name = name
     @token = token
-    end
+  end
 
   def token
     @token
@@ -18,54 +18,45 @@ end
 
 class Game
   def initialize()
-    @players = [Person.new('Player1', "X"), Person.new('Player2','O')]
-    @game_array = Array.new(9,' ')
+    @players = [Person.new(name = 'Player1', 'X'), Person.new(name = 'Player2', 'O')]
+    @game_array = Array.new(9, ' ')
     @current_player_index = 0
   end
 
   def print_game_board()
     @game_array.each_slice(3) do |row|
-        puts row.join('|') # Print each row as a comma-separated list
+      puts row.join('|') # Print each row as a comma-separated list
     end
   end
 
-  def InsertPlay(index, player)
+  def insert_play(index, player)
     if @game_array[index] == ' '
-      @game_array[index] = player
+      @game_array[index] = player.token
+      true
     else
       puts 'Error!, best try again'
+      false
     end
   end
 
-  def WinCheck()
+  def win_check()
     @game_array.each do |row|
     end
 
     3.times do |i|
       if @game_array[i] == @game_array[1] && @game_array[1] == @game_array[2]
-        WinConfirmation(game_array[i])
-        return true
+        win_confirmation(game_array[i])
+        true
       end
-      i += 3 #Work on proper loop method
     end
-    return false
+    false
   end
- 
-  def WinConfirmation(winners_token)
+
+  def win_confrimation(winners_token)
     if winners_token == @p1.token
       puts "#{@p1.name} WINNNNSSSSS"
     elsif winners_token == @p2.token
       puts "#{@p2.name} WINNNNSSSSS"
-    end
-  end
-
-  def GameRound()
-    rounds = 0
-    until rounds = 4
-      player1_index = prompt "#{player1.name} Choose your move(index 0-8): "
-      game.InsertPlay(player1_index,player1.token)
-      player2_index = prompt "#{player2.name} Choose your move(index 0-8): "
-      game.InsertPlay(player2_index,player2.token)
     end
   end
 
@@ -74,25 +65,26 @@ class Game
   end
 
   def play
-    9.times do
-      current_player = @players[@current_player_index]
-      loop do
-        position_index = prompt "#{current_player.name} Choose your move(index 0-8): "
-        break if @game_array[position_index] == ' '
-        puts "Error with input, Try Again between 0-8 Index"
-      end
-      if WinCheck == true
-        puts "Game Completed"
-      end
+    current_player = @players[@current_player_index]
+    loop do
+      position_index = prompt "#{@current_player} Choose your move(index 0-8): "
+      break if insert_play(position_index, current_player) == true
+
+      puts 'Error with input, Try Again between 0-8 Index'
+    end
+    switch_player
   end
-
-
 end
 
-def GamePlay()
+def game_play()
   game = Game.new()
-  game.GameRound()
-  game.print_game_board
+  9.times do
+    game.play
+    game.print_game_board
+    if game.win_check == true
+      puts 'Game Completed'
+    end
+  end
 end
 
-GamePlay()
+game_play
